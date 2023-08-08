0645b23df44caaeb2ff17c6eaae540deb779aac2
noahmeirelles <126571130+noahmeirelles@users.noreply.github.com>
2023-08-08 14:33:04 -0300

    feat: created story post function

---
+++ assets/js/script.posts.js
@@ -0,0 +1,35 @@
+const posts = [];
+function savePost(){
+    const title = document.getElementById("title").value;
+    const category = document.getElementById("category").value;
+    const resume = document.getElementById("resume").value;
+    const publisher = document.getElementById("publisher").value;
+    const date = document.getElementById("date").value;
+    console.log(title, category, resume, publisher, date);
+
+    if(title && category && resume && publisher && date){
+        storePost(title, category, resume, publisher, date);
+    cleanFields();
+    }else {
+        alert("Preencha todos os campos!")
+    }
+}
+function cleanFields(){
+    document.getElementById("title").value = "";
+    document.getElementById("category").value ="";
+    document.getElementById("resume").value = "";
+    document.getElementById("publisher").value = "";
+    document.getElementById("date").value = "";
+}
+function storePost(title, category, resume, publisher, date){
+    const post = {
+        title,
+        category,
+        resume,
+        publisher,
+        date
+    };
+    posts.push(post)
+
+    console.log(post)
+}
