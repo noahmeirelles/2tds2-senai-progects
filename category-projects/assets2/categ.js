4fc6fa5a6f51b55a7f709d7f42f50d597fdcec15
noahmeirelles <126571130+noahmeirelles@users.noreply.github.com>
2023-08-17 10:35:15 -0300

    essa vai

--- category-projects/assets2/categ.js
+++ category-projects/assets2/categ.js
@@ -6,4 +6,24 @@ class Category{
         this.products = []
     }
 }
-
+class Product{
+    constructor(id, name, price, category){
+        this.id = id
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.products = []
+    }
+}
+ class CategoryService {
+    constructor(){
+        this.categories = [];
+        this.nextCategoryId = 0;
+    }
+    
+    addCategory(name){
+        const id = this.nextCategoryId ++;
+        const category = new Category(id, name);
+        this.categories.push(category);
+    }
+ }
