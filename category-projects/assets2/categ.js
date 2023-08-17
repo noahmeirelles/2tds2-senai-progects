a0182b903a9ba4075d3ae3ea858a11798865a72b
noahmeirelles <126571130+noahmeirelles@users.noreply.github.com>
2023-08-17 17:09:02 -0300

    feat: add function displayCategoryAndProducts

--- category-projects/assets2/categ.js
+++ category-projects/assets2/categ.js
@@ -1,67 +1,94 @@
 //categoria possui varios produtos => 1:N
-class Category{
-    constructor(id, name){
-        this.id = id
-        this.name = name;
-        this.products = []
+class Category {
+     constructor(id, name) {
+      this.id = id;
+      this.name = name;
+      this.products = [];
+     }
     }
-}
-//produto pertence a uma categoria =>1:1
-class Product{
-    constructor(id, name, price, category){
-        this.id = id
-        this.name = name;
-        this.price = price;
-        this.category = category;
-        this.products = []
+    class Products {
+     constructor(id, name, price, category) {
+      this.id = id;
+      this.name = name;
+      this.price = price;
+      this.category = category;
+     }
     }
-}
- class CategoryService {
-    constructor(){
-        this.categories = [];
-        this.nextCategoryId = 0;
+    class CategoryService {
+     constructor() {
+      this.categories = [];
+      this.nextCategoryId = 1;
+     }
+     addCategory(name) {
+      const id = this.nextCategoryId++;
+      const category = new Category(id, name);
+      this.categories.push(category);
+     }
     }
+    class ProductService {
+     constructor() {
+      this.products = [];
+      this.nextProductId = 0;
+     }
+     addProduct(name, price, category) {
+      const id = this.nextProductId++;
+      const product = new Products(id, name, price, category);
+      category.products.push(product);
+      this.products.push(product);
+     }
+    }
+    const categoryList = new CategoryService();
+    const productList = new ProductService();
     
-    addCategory(name){
-        const id = this.nextCategoryId ++;
-        const category = new Category(id, name);
-        this.categories.push(category);
+    function createCategory() {
+     const categorieName = document.getElementById("categoryName").value;
+     categoryList.addCategory(categorieName);
+     displayCategoriesAndProducts()
+     clearFormFields();
+     console.log(categoryList.categories);
     }
- }
- class ProductService{
-    constructor(){
-        this.products = [];
-        this.nextProductId = 1;
+    function createProduct() {
+     const productName = "Bolo";
+     const productPrice = "20";
+     const productCategory = categoryList.categories[0];
+     productList.addProduct(productName, productPrice, productCategory);
+     console.log(productList.products);
     }
-    addProduct(name, price, category){
-        const id = this.nextProductId++;
-        const product = new Product(id, name,  price, category);
-        category.products.push(product);
-        this.products.push(product);
+    function clearFormFields() {
+     document.getElementById("categoryName").value = "";
+     document.getElementById("productName").value = "";
+     document.getElementById("productPrice").value = "";
+     document.getElementById("productCategory").value = "";
     }
- }
-
- const categoryList = new CategoryService();
- const productList = new ProductService();
-
- function createCategory(){
-    const categoryName = document.getElementById("categoryName").value;
-    categoryList.addCategory(categoryName);
-    //console.log(categoryList.categories);
- }
-
- function createProduct(){
-    const productName = "Bolo";
-    const productPrice = 20;
-    const productCategory = categoryList.categories[0];
-
-    productList.addProduct(productName, productPrice, productCategory);
-    //console.log(productList.products);
- }
-
- function clearFormfields(){
-    document.getElementById("categoryName").value;
-    document.getElementById("productName").value;
-    document.getElementById("productPrice").value;
-    document.getElementById("productCategory").value;
- }
+    function displayCategoriesAndProducts() {
+     let content = "";
+     categoryList.categories.forEach((category) => {
+      content += `
+      <li>
+      <div class="categoriesList">
+      <span><b>Categoria:</b>${category.name}</span>
+      <div>
+      <button class="editButton">Editar</button>
+      <button class="deleteButton">Remover</button>
+      </div>
+      </div>
+      <ul class="productListByCategory>`;
+      category.products.forEach((product) =>{
+       content+=`
+       <li>
+       <div class="productsList">
+       <span><b>Produto:</b> ${product.name} - <b>Preço:</b></span>
+       <div>
+       <button class="editButton">Editar</button>
+       <button class="deleteButton">Editar</button>
+       </div>
+       </div>
+       `;
+      });
+      content +=`
+      </ul>
+      </li>
+      `
+     });
+     document.getElementById("categoriesList").innerHTML = content;
+    }
